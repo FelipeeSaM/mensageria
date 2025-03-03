8a77da8ee657b34356a0e76880cef790b6b3d60c
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-03-03 20:52:51 -0300

    Criando api

---
+++ EasyNetQ.Customer.API/Models/CustomerCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyNetQ.Customers.API.Models
+{
+    public class CustomerCreated
+    {
+        public CustomerCreated(int id, string? name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
