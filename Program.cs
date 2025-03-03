089d04b4b9f3a2043174c8d41c6d70fe8827f9a9
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-03-03 00:35:12 -0300

    consimondo ?

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 using System.Text.Json;
 
@@ -6,13 +7,13 @@ const string EXCHANGE = "curso-rabbitmq";
 
 var person = new Person ("Eu mesmo", "1234567", new DateTime(1993, 12, 1));
 
-
+#region publicando
 var connectionFactory = new ConnectionFactory 
 {
     HostName = "localhost",
 };
 
-var connection = connectionFactory.CreateConnection("nomeDaConexao");
+var connection = connectionFactory.CreateConnection();
 
 var channel = connection.CreateModel();
 
@@ -22,7 +23,25 @@ var arrayBytes = Encoding.UTF8.GetBytes(json);
 
 channel.BasicPublish(EXCHANGE, "hr.routing-key", null, arrayBytes);
 
-Console.WriteLine(json);
+Console.WriteLine($"Publicando mensagem: {json}");
+#endregion
+
+#region consumindo
+var consummerChannel = connection.CreateModel();
+var consumer = new EventingBasicConsumer(consummerChannel);
+
+consumer.Received += (sender, eventArgs) => {
+    var contentArray = eventArgs.Body.ToArray();
+    var jsonString = Encoding.UTF8.GetString(contentArray);
+
+    var message = JsonSerializer.Deserialize<Person>(jsonString);
+
+    Console.WriteLine($"recebendo mensagem: {jsonString}");
+
+    consummerChannel.BasicAck(eventArgs.DeliveryTag, false);
+};
+
+#endregion
 
 Console.ReadLine();
 
