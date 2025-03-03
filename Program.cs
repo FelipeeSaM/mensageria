c1c48493fd432bc52cf192eb29325a4006bee687
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-03-02 23:39:39 -0300

    enviando mensagem

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
-﻿namespace mensageria {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+const string EXCHANGE = "curso-rabbitmq";
+
+var person = new Person ("Eu mesmo", "1234567", new DateTime(1993, 12, 1));
+
+
+var connectionFactory = new ConnectionFactory 
+{
+    HostName = "localhost",
+};
+
+var connection = connectionFactory.CreateConnection("nomeDaConexao");
+
+var channel = connection.CreateModel();
+
+var json = JsonSerializer.Serialize(person);
+
+var arrayBytes = Encoding.UTF8.GetBytes(json);
+
+channel.BasicPublish(EXCHANGE, "hr.routing-key", null, arrayBytes);
+
+Console.WriteLine(json);
+
+Console.ReadLine();
+
+class Person {
+    public Person(string fullName, string document, DateTime birthday) {
+        FullName = fullName;
+        Document = document;
+        Birthday = birthday;
     }
+    public string FullName { get; set; }
+    public string Document { get; set; }
+    public DateTime Birthday { get; set; }
 }
