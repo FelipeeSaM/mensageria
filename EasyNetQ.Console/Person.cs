e6471e9b41908d31284a50352b0c5c3c9e2ec136
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-03-03 21:01:47 -0300

    Add projeto console

---
+++ EasyNetQ.Console/Person.cs
@@ -0,0 +1,21 @@
+﻿using EasyNetQ.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyNetQ.Console {
+    public class Person {
+        public Person(string fullName, string document, DateTime birthday) {
+            FullName = fullName;
+            Document = document;
+            Birthday = birthday;
+        }
+
+        public string FullName { get; set; }
+        public string Document { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ EasyNetQ.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using EasyNetQ;
+using EasyNetQ.Console;
+using Newtonsoft.Json;
+
+const string EXCHANGE = "curso-rabbitmq";
+const string QUEUE = "person-created";
+const string ROUTING_KEY = "hr.person-created";
+
+var person = new Person("Eu mesmo", "1234567", new DateTime(1992, 12, 1));
+
+// Criando canal
+var bus = RabbitHutch.CreateBus("host=localhost");
+
+#region mais rapido
+//// Publicando mensagem
+//await bus.PubSub.PublishAsync(person);
+
+//// Consumindo mensagem
+//await bus.PubSub.SubscribeAsync<Person>("marketing", msg => 
+//    {
+//    var json = JsonConvert.SerializeObject(msg);
+//    Console.WriteLine(json);
+//    }
+//);
+#endregion
+
+
+// Tendo maior controle
+var advanced = bus.Advanced;
+
+// publicando
+var exchange = advanced.ExchangeDeclare(EXCHANGE, "topic");
+var queue = advanced.QueueDeclare(QUEUE);
+
+advanced.Publish(exchange, ROUTING_KEY, true, new Message<Person>(person));
+
+// consumindo
+advanced.Consume<Person>(queue, (msg, info) => {
+    var json = JsonConvert.SerializeObject(msg.Body);
+    Console.WriteLine("dsadsa");
+    Console.WriteLine(json);
+});
+
+Console.ReadLine();
